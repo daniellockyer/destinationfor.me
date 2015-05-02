d458e6b16938f6d8e235bf7882afc283747b20ff
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2015-05-02 19:55:56 +0100

    Updates

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var router = express.Router();
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("production.db");
 
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-var router = express.Router();
+router.get('/', function(req, res) { 
+	var jobs = [];
 
-router.get('/', function(req, res) {
-res.render('home');
+	db.serialize(function() {
+		db.each("SELECT * FROM jobs", function(err, row) {
+	    	jobs.push(row.title);
+		}, function() {
+			res.render('home', { data: jobs }); 
+		});
+	});
 });
+router.get('/uni', function(req, res) { 
+	var unis = [];
 
-router.get('/uni/:id', function(req, res) {
-res.render('uni', { id: req.params.id });
+	db.serialize(function() {
+		db.each("SELECT * FROM universities", function(err, row) {
+	    	unis.push(row);
+		}, function() {
+			res.render('unilist', { data: unis }); 
+		});
+	});
 });
-
-router.get('/people', function(req, res) {
-	res.render('people');
+router.get('/uni/:id', function(req, res) { 
+	db.serialize(function() {
+		db.each("SELECT * FROM universities WHERE id="+req.params.id, function(err, uni) {
+	    	res.render('uni', { data: uni }); 
+		});
+	});
 });
-
-router.get('/add', function(req, res) {
-	res.render('add');
-});  
-
-router.get('/tables', function(req, res) {
-	res.render('tables');
-});  
-
-router.get('/find', function(req, res) {
-	res.render('find');
-});  
+router.get('/people', function(req, res) { res.render('people'); });
+router.get('/add', function(req, res) { res.render('add'); });  
+router.get('/tables', function(req, res) { res.render('tables'); });  
+router.get('/find', function(req, res) { res.render('find'); });  
 
 app.use(router);
-
 app.listen(80);
--- db.js
+++ db.js
@@ -3,9 +3,10 @@ var file = "production.db";
 fs.unlinkSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
+var request = require('request');
 
 db.serialize(function() {
-    db.run("CREATE TABLE universities (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, wikipedia TEXT, logo TEXT, website TEXT, background TEXT);");
+    db.run("CREATE TABLE universities (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);");
     db.run("CREATE TABLE jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);");
     db.run("CREATE TABLE courses (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);");
     db.run("CREATE TABLE jobslist (userid INT, jobid INT, companyid INT);");
@@ -20,12 +21,34 @@ db.serialize(function() {
 
 	  	for(i in lines) {
 	  		var line = lines[i];
-
 	  		db.run("INSERT INTO courses (title) VALUES (\""+line+"\");");
 	  	}
 	});
-});
 
-/*db.each("SELECT * FROM courses", function(err, row) {
-    console.log(row);
-});*/
+    fs.readFile('data/jobs.txt', 'utf8', function (err,data) {
+        if (err) return console.log(err);
+        var lines = data.split("\n");
+
+        for(i in lines) {
+            var line = lines[i];
+            db.run("INSERT INTO jobs (title) VALUES (\""+line+"\");");
+        }
+    });
+
+    fs.readFile('data/unis.txt', 'utf8', function (err,data) {
+        if (err) return console.log(err);
+        var lines = data.split("\n");
+
+        for(i in lines) {
+            var line = lines[i];
+
+           /* request('https://en.wikipedia.org/wiki/'+line.replace(" ","_"), function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                console.log(body)
+              }
+            });*/
+
+            db.run("INSERT INTO universities (title) VALUES (\""+line+"\");");
+        }
+    });
+});
